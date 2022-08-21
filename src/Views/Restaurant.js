70bf5209a60353e768ac3c523651d77d64ed1be4
DevHumbleChris <dev.humblechris254@gmail.com>
2022-08-21 06:32:02 +0300

    Added "Add to Restaurant Component"

---
+++ src/Views/Restaurant.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import RestaurantBanner from '../components/RestaurantBanner'
+
+export default function Restaurant() {
+  return (
+    <main>
+        <RestaurantBanner />
+    </main>
+  )
+}
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Homepage from '../Views/Homepage'
-import Footer from './Footer'
-import Navbar from './Navbar'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../Views/Homepage";
+import Restaurant from "../Views/Restaurant";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
 
 export default function Layout() {
   return (
     <BrowserRouter>
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<Homepage />} />
-        </Routes>
-        <Footer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/add-restaurant" element={<Restaurant />} />
+      </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
